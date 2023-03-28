072c57b9d3516e5a60e0a1d019c3b52c97442c9a
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-03-28 02:47:25 -0300

    made some changes

--- react/frontend/src/App.jsx
+++ react/frontend/src/App.jsx
@@ -1,14 +1,42 @@
 import { useState } from 'react'
 import './App.css'
-import { Typography, TextField, Button } from "@mui/material"
+import { Box, Typography, TextField, Button } from "@mui/material"
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  function handleClick() {
+    setCount(prevCount => prevCount + 1)
+  }
+
+  const [greetName, setGreetName] = useState("")
+
   return (
     <div className="App">
-      <Typography variant="h4">Hello, World</Typography>
-      <TextField variabt="outlined-required" label="name" id="name" placeholder="John Doe" />
       <Typography variant="h2">I am Bhavik</Typography>
       <Typography variant="body1">Welcome to my website!</Typography>
+
+      <Box
+        component="form"
+        sx={{
+          '& .MuiTextField-root': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField required
+          label="name"
+          id="name-required"
+          value={greetName}
+          onChange={e => setGreetName(e.target.value)}
+        />
+        {greetName !== "" && <Typography variant="h4">Hello, {greetName}</Typography>}
+      </Box>
+
+      <Box>
+        <Typography variant="h5">Count: {count}</Typography>
+        <Button variant="contained" size="large" onClick={handleClick}>count</Button>
+      </Box>
     </div>
   )
 }
