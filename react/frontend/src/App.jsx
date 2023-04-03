6ce0a4c479c733565727e179b822fb2f18d242c4
Bhavik <15388512+bhavik-knight@users.noreply.github.com15388512+bhavik-knight@users.noreply.github.com>
2023-04-03 16:41:06 -0300

    success: working navbar

--- react/frontend/src/App.jsx
+++ react/frontend/src/App.jsx
@@ -1,11 +1,27 @@
 import "./App.css";
+import { useState } from "react";
 import { Page } from "./components/Page.jsx";
 import { createTheme, ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 
 function App() {
+  const darkTheme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
+
+  // to check the dark mode
+  const [darkMode, setDarkMode] = useState(false)
 
   return (
-    <Page />
+    <ThemeProvider theme={darkTheme}>
+      {darkMode && <CssBaseline />}
+      < Page
+        currentDarkMode={darkMode}
+        setDarkMode={setDarkMode}
+      />
+    </ThemeProvider>
   )
 }
 
--- react/frontend/src/components/Navbar.jsx
+++ react/frontend/src/components/Navbar.jsx
@@ -1,13 +1,31 @@
+import "./Navbar.css";
 import { useState } from "react";
-import MenuIcon from '@mui/icons-material/Menu';
-import { AppBar, Box, IconButton, Button, ButtonGroup, Toolbar } from '@mui/material';
+import MenuIcon from "@mui/icons-material/Menu";
+import BrightNess4Icon from "@mui/icons-material/Brightness4";
+import BrightNess7Icon from "@mui/icons-material/Brightness7";
+import Divider from "@mui/material/Divider";
+import { AppBar, Box, Button, ButtonGroup, SvgIcon, Toolbar, ToggleButton } from '@mui/material';
 
-function ButtonAppBar(props) {
-    const control = {
+
+function HomeIcon(props) {
+    return (
+        <SvgIcon {...props}>
+            <path d="M10 20v-6h4v6h5v-8h3L12 3 2 12h3v8z" />
+        </SvgIcon>
+    );
+}
+
+function Navbar(props) {
+    const controls = {
         color: "white",
-        border: "1px solid white",
+        marginInline: 1,
+        backgroundColor: "inherit",
+        border: "2px solid white",
     }
 
+
+    // reference for button-group-functionality:
+    // https://dev.to/ramonak/react-how-to-create-a-custom-button-group-component-in-5-minutes-3lfd
     const [selectedButton, setSelectedButton] = useState(-1)
 
     function handleClick(event, i) {
@@ -15,42 +33,67 @@ function ButtonAppBar(props) {
         props.onClick(event)
     }
 
-    return (
-        <Box sx={{ flexGrow: 1, p: 0, m: 0 }} >
-            <AppBar position="static" sx={{ bgcolor: "black" /*transaparent*/ }}>
-                <Toolbar sx={{ display: "flexbox" }}>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        onClick={() => console.log("menu is cliked")}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <ButtonGroup
-                        variant={props.variant}
-                        size={props.size}
-                    >
-                        {
-                            props.buttonList.map((button, index) => (
-                                // console.log(`narvar-button: ${button}`)
-                                <Button
-                                    key={index}
-                                    onClick={(event) => handleClick(event, index)}
-                                    name={button}
-                                    className={index === selectedButton ? "navbar.button:selected" : "navbar.button"}
-                                >
-                                    {button}
-                                </Button>
-                            ))
-                        }
+    // check the current dark mode value; change it on the button click and pass it to the parents
+    // reference: https://stackoverflow.com/questions/40722382/how-to-pass-state-back-to-parent-in-react
+    let currentDarkModeValue = props.currentDarkMode
 
-                    </ButtonGroup>
-                </Toolbar>
-            </AppBar>
-        </ Box >
+    // this will be return to be rendered in Page
+    return (
+        <AppBar
+            enableColorOnDark
+            position="static"
+            sx={{ backgroundColor: "blue" }}
+        >
+            <Toolbar sx={{ ...controls, m: 0 }} component="div">
+                {/* 1. menu-button */}
+                <Button
+                    sx={{ ...controls }}
+                    aria-label="menu"
+                    onClick={() => console.log("menu is cliked")}
+                >
+                    <MenuIcon />
+                </Button>
+
+                {/* 3. pages */}
+                <ButtonGroup
+                    size={"large"}
+                    orientation="horizontal"
+                    sx={{ ...controls, flexGrow: 1 }}
+                >
+                    {
+                        props.buttonList.map((button, index) => (
+                            // console.log(`narvar-button: ${button}`)
+                            <Button
+                                key={index}
+                                onClick={(event) => handleClick(event, index)}
+                                name={button}
+                                className={index === selectedButton ? "navbar.button:selected" : "navbar.button"}
+                            >
+                                {button}
+                            </Button>
+                        ))
+                    }
+                </ButtonGroup>
+
+                {/* create a virual divider to push dark-theme icon to the right}
+                    <Divider />
+
+                    {/* 3. dark-theme */}
+                <ToggleButton
+                    disableTouchRipple
+                    disableFocusRipple
+                    disableRipple
+                    value={currentDarkModeValue}
+                    name="darkMode"
+                    className="dark-mode ms-auto"
+                    sx={{ ...controls, border: "none" }}
+                    onChange={() => { props.setDarkMode(!currentDarkModeValue) }}
+                >
+                    {currentDarkModeValue ? <BrightNess7Icon /> : <BrightNess4Icon />}
+                </ToggleButton>
+            </Toolbar>
+        </AppBar>
     );
 }
 
-export { ButtonAppBar };
+export { Navbar };
